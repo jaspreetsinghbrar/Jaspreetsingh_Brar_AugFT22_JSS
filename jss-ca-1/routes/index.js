4835aee5d91f6d0831223c1453edeb7bc2305cbf
Jas Brar <jas.brar@ymail.com>
2023-01-25 20:16:39 +0100

    Added memes table

--- jss-ca-1/routes/index.js
+++ jss-ca-1/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var memes = require("./memes");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get("/", function (req, res, next) {
+  res.render("index", { memes: global.memes });
 });
+
+router.use("/memes", memes);
 
 module.exports = router;
--- jss-ca-1/routes/memes.js
+++ jss-ca-1/routes/memes.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 // const { resolve } = require('path');
-const axios = require('axios');
+const axios = require("axios");
 
 //BM New API endpoint to use for the data from the soccer API
-router.get('/', function (req, res, next) {});
+router.get("/", (req, res) => {
+  res.render("memes", { memes: global.memes });
+});
 
 module.exports = router;
